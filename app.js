8df911a356f83d23997908f264bed3e1fa57a9e0
Michał Zając <michal.zajac@gmail.com>
2024-01-18 13:09:53 +0000

    Initial version

---
+++ app.js
@@ -0,0 +1,27 @@
+document.addEventListener("DOMContentLoaded", function(event) {
+  // show overlay when clicking on the photo
+  document.querySelectorAll(".gallery > figure").forEach((element) => {
+    element.addEventListener("click", () => {
+      const overlay = element.querySelector(".overlay");
+      overlay.classList.toggle("display-none");
+    });
+  });
+
+  // show map overlay when clicking on the Details link
+  document.querySelectorAll(".details").forEach((element) => {
+    element.addEventListener("click", () => {
+      const figure = element.closest("figure");
+      const mapOverlay = figure.querySelector(".map");
+      mapOverlay.classList.toggle("display-none");
+    });
+  });
+
+  // hide map overlay when clicking anywhere
+  document.querySelectorAll(".map").forEach((mapOverlay) => {
+    mapOverlay.addEventListener("click", () => {
+      ["display-none", "fadein"].forEach(klass => {
+        mapOverlay.classList.toggle(klass);
+      });
+    })
+  });
+});
